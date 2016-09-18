27133a95e23a07d7d17983dcf2c0efdcc260075b
Ahmed Metwally <ametwa2@uic.edu>
2016-09-17 20:09:59 -0500

    add test cases for all Halstead metrics

--- src/test/java/HalsteadTest.java
+++ src/test/java/HalsteadTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
  */
 
 /**
- * @author Ahmed
+ * @author Ahmed Metwally
  *
  */
 public class HalsteadTest {
@@ -25,5 +25,49 @@ public class HalsteadTest {
 		testHal.setParameters(10, 7, 16, 15);
 		assertEquals(31, testHal.getProglen());
 	}
-
+	
+	@Test
+	public void testgetVolume() {		
+		HalsteadMetrics testHal = new HalsteadMetrics();
+		testHal.setParameters(10, 7, 16, 15);		
+		System.out.println("getVolume= "+ (double)Math.round(testHal.getVolume()));
+		assertEquals(127, Math.round(testHal.getVolume()));
+	}
+	
+	
+	@Test
+	public void testgetDifficulty() {		
+		HalsteadMetrics testHal = new HalsteadMetrics();
+		testHal.setParameters(10, 7, 16, 15);		
+		System.out.println("getDifficulty= "+ (double)Math.round(testHal.getDifficulty()));
+		assertEquals(11, Math.round(testHal.getDifficulty()));
+	}
+	
+	
+	@Test
+	public void testgetEffort() {		
+		HalsteadMetrics testHal = new HalsteadMetrics();
+		testHal.setParameters(10, 7, 16, 15);		
+		System.out.println("getEffort= "+ (double)Math.round(testHal.getEffort()));
+		assertEquals(1358, Math.round(testHal.getEffort()));
+	}
+	
+	
+	
+	@Test
+	public void testgetTimeReqProg() {		
+		HalsteadMetrics testHal = new HalsteadMetrics();
+		testHal.setParameters(10, 7, 16, 15);		
+		System.out.println("getTimeReqProg= "+ (double)Math.round(testHal.getTimeReqProg()));
+		assertEquals(75, Math.round(testHal.getTimeReqProg()));
+	}
+	
+	
+	@Test
+	public void testgetTimeDelBugs() {		
+		HalsteadMetrics testHal = new HalsteadMetrics();
+		testHal.setParameters(10, 7, 16, 15);		
+		System.out.println("getTimeDelBugs= "+ (double)Math.round(testHal.getTimeDelBugs()));
+		assertEquals(0, Math.round(testHal.getTimeDelBugs()));
+	}
 }
